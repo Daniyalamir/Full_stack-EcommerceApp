ec4b4c66b85758f482696d783d5cc494ffa2f202
Daniyalamir <udanial555@gmail.com>
2023-08-18 05:41:16 -0700

    adding productCarousalFolder

--- src/app/components/shared/Wrapper/BasePath.ts
+++ src/app/components/shared/Wrapper/BasePath.ts
@@ -1,4 +1,5 @@
-const BASE_PATH_FORAPI =
+const BASE_PATH_FORAPI = process.env.NODE_ENV === "development" ?
 "http://localhost:3001" 
+: "https://full-stack-ecommerce-app-five.vercel.app"
 
 export default BASE_PATH_FORAPI 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,27 @@ import React from 'react'
 import Wrapper from '@/app/components/shared/Wrapper'
 import Hero from '@/app/components/views/Hero'
 import ProductType from '@/app/components/views/ProductTypes'
-// import BASE_PATH_FORAPI from './components/shared/Wrapper/BasePath'
+import BASE_PATH_FORAPI from '@/app/components/shared/Wrapper/BasePath'
+import ProductCarousal from '@/app/components/views/ProductCarousal'
 
 
 
-// async function fetchAllProductsData(){
-//    let response = await fetch(`${BASE_PATH_FORAPI}/api/products`)
-//     if(!response.ok){
-//       throw new Error("Failed to fetch");
-//     }
-//     return response.json()
-//   }
-export default function Home() {
-  // let res = await fetchAllProductsData();
-  // console.log("response :" , res);
+async function fetchAllProductsData(){
+   let response = await fetch(`${BASE_PATH_FORAPI}/api/products`)
+    if(!response.ok){
+      throw new Error("Failed to fetch");
+    }
+    return response.json()
+  }
+export default async function Home() {
+  let {res}  = await fetchAllProductsData();
+  console.log("response :" , res);
 
   return (
     <div>
     <Hero/>
     <ProductType/>
+    <ProductCarousal ProductData ={res}/>
     </div>
   )
-}
+  }
